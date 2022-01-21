4d7e5957fa320c19002b3523c7234d420edcdf01
vibo777 <54715728+vibo777@users.noreply.github.com>
2022-01-21 14:18:32 +0530

    update

--- video-streaming-app/src/components/Register.js
+++ video-streaming-app/src/components/Register.js
@@ -1,16 +1,34 @@
 function Register(){
+    
+
+    let user={};
+    // function to read vlaue of input field
+    function readvalue(property,value){
+        user[property]=value; 
+    }  
+
+    // register
+    function register(){
+        if(user.cpassword===user.password){
+            delete user.cpassword;
+            console.log(user);
+        } 
+        else{
+            console.log("Password don't match");
+        }
+    }
 
     return(
         <section>
             <div className="main">
                 <div className="register">
-                    <h1>Create an account</h1>
-                    <input type="text" placeholder="Enter your name"/>
-                    <input type="email" placeholder="Enter your Email"/>
-                    <input type="text" placeholder="Enter your username"/>
-                    <input type="password" placeholder="Enter your password"/>
-                    <input type="password" placeholder="Confirm your password"/>
-                    <button>Register</button>    
+                    <h1 className="title">Create an account</h1>
+                    <input type="text" placeholder="Enter your name" onChange={(event)=>{readvalue("name", event.target.value)}}/>
+                    <input type="email" placeholder="Enter your Email" onChange={(event)=>{readvalue("email",event.target.value)}}/>
+                    <input type="text" placeholder="Enter your username" onChange={(event)=>{readvalue("username",event.target.value)}}/>
+                    <input type="password" placeholder="Enter your password" onChange={(event)=>{readvalue("password",event.target.value)}}/>
+                    <input type="password" placeholder="Confirm your password" onChange={(event)=>{readvalue("cpassword",event.target.value)}}/>
+                    <button onClick={register}>Register</button>    
                 </div>  
             </div>
         </section>
