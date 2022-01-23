2cb57ac33c58967a64316902709f8fb083a31154
vibo777 <54715728+vibo777@users.noreply.github.com>
2022-01-23 23:34:42 +0530

    update

--- video-streaming-app/src/components/Video.js
+++ video-streaming-app/src/components/Video.js
@@ -9,6 +9,23 @@ function Videos(){
                     <div className="video-img">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg2tkrCP9lZxzQ9V4OHngvQGuUsEhot2qZwQ&usqp=CAU"/> 
                     </div>    
+                    <div className="padd">
+                        <h1>Money Heist</h1>
+                        <p>
+                            Action . Thriller . Drama
+                        </p>    
+                        <p>
+                           IMDB : 9.2     
+                        </p>
+                        <button className="btn">Watch Now</button>
+
+
+                    </div>
+
+
+
+
+
                 </div>
             </div>
         
