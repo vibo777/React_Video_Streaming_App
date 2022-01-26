eec8653c0afaec6c5137213a823f37d7b1c31404
vibo777 <54715728+vibo777@users.noreply.github.com>
2022-01-26 18:13:46 +0530

    update

--- video-streaming-app/src/components/Videoplayer.js
+++ video-streaming-app/src/components/Videoplayer.js
@@ -1,10 +1,10 @@
-import { useEffect,useRef,useState} from 'react';
 import {useParams} from 'react-router-dom'
+import { useEffect,useRef,useState} from 'react';
 
 function Videoplayer(){
 
     const params = useRef(useParams());
-    console.log(params);
+    // console.log(params);
 
     let [video,setVideo]=useState({});
     let token = useRef(JSON.parse(localStorage.getItem("vs_details")).token);
@@ -17,8 +17,8 @@ function Videoplayer(){
         })
         .then((response)=>response.json())
         .then((data)=>{
-            console.log(data);
-            setVideo(data);
+            console.log(data[0]);
+            setVideo(data[0]);
         })
         .catch((err)=>{
             console.log(err);
@@ -28,7 +28,66 @@ function Videoplayer(){
 
     return(
         <div className="bg">
-            <h1>Welcome to Video player</h1>
+            <h1 className='title'>{video.originalTitle}</h1>
+
+            <div className='video_area'>
+
+                <video width="100%" controls>
+
+                </video>
+
+                <div className='content'>
+
+                    <div className='storyline'>
+                        <h1 className='title'>Storyline</h1>
+                        {video.storyline}
+                    </div>
+
+                    <div className='rating'> 
+                        <h1 className='title'>IMDB Rating</h1>
+                        {video.imdbRating} 
+                    </div>
+
+                    <div className='genre'>
+                    <h1 className='title'>Genre</h1>    
+                        {
+                            video.genres?.map((genre,index)=>{
+                                return(
+                                    <p>
+                                        <span key={index}>{genre}.</span>    
+                                    </p>
+                                )
+                            })
+                        }     
+                    </div>    
+
+                    <div className='actors'>
+                    <h1 className='title'>Actors</h1>    
+                        {
+                            video.actors?.map((actor,index)=>{
+                                return(
+                                   <p>
+                                       <span key={index}>{actor}.</span> 
+                                   </p> 
+                                       
+                                )
+                            })
+                        }     
+                    </div>    
+
+                    <div className='duration'> 
+                        <h1 className='title'>Duration</h1>
+                        {video.duration} miniutes 
+                    </div>
+
+
+ 
+                </div>
+
+            </div>
+
+
+
         </div>
     )
 
